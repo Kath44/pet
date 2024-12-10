215f188851c49cd62f02b001bf253f7c301d4cb9
Kath44 <kathrepunte44@gmail.com>
2024-12-10 20:42:29 +0800

    dec 10

---
+++ pages/frontend/frontend.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include '../includes/header.php' ?>
+
+<body class="hold-transition layout-top-nav">
+<div class="wrapper">
+
+<?php include 'navbar.php' ?>
+
+  
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0"> Home Page </h1>
+          </div><!-- /.col -->
+          
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+      <div class="container">
+        <div class="row">
+          <!-- /.col-md-6 -->
+          <div class="col-lg-12">
+            <div class="card">
+              <div class="card-header">
+                <h5 class="card-title m-0">Pet Adoption System</h5>
+              </div>
+              <div class="card-body">
+                <p class="card-text">
+                Pet adaptation refers to the process of helping a pet adjust to new environments, routines, or changes in their life. This can include moving to a new home, introducing them to new people or animals, or adapting to shifts in schedules or household dynamics.                </p>
+                <p class="card-text">
+                Proper adaptation is essential for ensuring a pet’s emotional well-being and minimizing stress-related behaviors, such as excessive barking, scratching, chewing, or withdrawal. When transitioning to a new home, it’s important to create a welcoming space with familiar items like their bed, toys, and food bowls while allowing them to explore gradually and maintaining a consistent routine to foster a sense of normalcy                </p>
+                <p class="card-text">
+                Introducing pets to new people or animals requires patience and supervision, ensuring interactions are gradual, positive, and respectful of the pet’s boundaries. Adjusting to changes in routines should be done incrementally, offering enrichment activities such as toys or puzzles to keep them engaged. For pets experiencing anxiety, providing a safe space, using calming tools like pheromone diffusers, and engaging in soothing activities can help them feel secure. Early training and socialization also play a vital role in helping pets adapt to different settings and experiences, building their confidence and resilience. Although adaptation may come with challenges such as separation anxiety or reluctance to eat, patience, positive reinforcement, and professional guidance, if necessary, can make the process smoother. With love, consistency, and reassurance, pets can adjust successfully to changes and thrive in their new environments.
+                </p>
+              </div>
+            </div>
+          </div>
+          <!-- /.col-md-6 -->
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+
+  <!-- Main Footer -->
+  <?php include '../includes/dashboard_footer.php' ?>
+</div>
+<!-- ./wrapper -->
+
+</body>
+</html>
